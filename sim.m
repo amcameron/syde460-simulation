5b551452af09159a81da49a7e88cc0217ad6f365
Andrew Cameron <amcameron@gmail.com>
2010-11-01 20:23:42 -0400

    fix typo; name states and inputs

--- sim.m
+++ sim.m
@@ -19,11 +19,20 @@ A_lat = [-0.2195  -0.1580 -10.7980 9.722 -1.3098;
 	  0        1        0      0      0;
 	  0        0        1      0      0];
 B_lat = [0; 3.6136; -0.4311; 0; 0];
-sys_long = ss(A_lat, B_lat, eye(5)); % output all state variables for now
+sys_lat = ss(A_lat, B_lat, eye(5)); % output all state variables for now
 
 % Entire system all at once!
 A = blkdiag(A_long, A_lat);
 B = blkdiag(B_long, B_lat);
 C = eye(9); % output all state variables for now
+D = [];
+Tsam = 0;
+Ncontin = rows(A);
+Ndiscr = 0;
+STNAME = {"axial velocity", "normal velocity", "pitch rate", ...
+	"pitch attitude", "lateral velocity", "roll rate", "yaw rate", ...
+	"roll attitude", "yaw attitude"};
+INNAME = {"longitudinal control angle", "lateral control angle"};
+sys = ss(A, B, C, D, Tsam, Ncontin, Ndiscr, STNAME, INNAME, STNAME);
 
 % TODO: figure out how to apply a step (i.e. what is meaningful to step?)
