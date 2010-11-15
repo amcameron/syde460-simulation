e41b146e9cca60d5705240eb62d861d57d3536ef
Peter Lobsinger <plobsing@gmail.com>
2010-11-15 16:45:59 -0500

    generic changes to sim.m

--- sim.m
+++ sim.m
@@ -7,17 +7,17 @@
 % Longitudinal equations:
 A_long = [-0.1730  0.6538  0.1388 -9.7222;
           -1.4208 -2.2535 10.7370  1.3093;
-	   0.2685 -0.4402 -1.4113  0;
-	   0       0       1       0];
+           0.2685 -0.4402 -1.4113  0;
+           0       0       1       0];
 B_long = [0; 0; 7.46; 0];
 sys_long = ss(A_long, B_long, eye(4)); % output all state variables for now
 
 % Lateral equations:
 A_lat = [-0.2195  -0.1580 -10.7980 9.722 -1.3098;
          -1.4670 -21.3180   7.5163 0      0;
-	  0.2906   3.7362  -2.1119 0      0;
-	  0        1        0      0      0;
-	  0        0        1      0      0];
+          0.2906   3.7362  -2.1119 0      0;
+          0        1        0      0      0;
+          0        0        1      0      0];
 B_lat = [0; 3.6136; -0.4311; 0; 0];
 sys_lat = ss(A_lat, B_lat, eye(5)); % output all state variables for now
 
@@ -30,9 +30,11 @@ Tsam = 0;
 Ncontin = rows(A);
 Ndiscr = 0;
 STNAME = {"axial velocity", "normal velocity", "pitch rate", ...
-	"pitch attitude", "lateral velocity", "roll rate", "yaw rate", ...
-	"roll attitude", "yaw attitude"};
+        "pitch attitude", "lateral velocity", "roll rate", "yaw rate", ...
+        "roll attitude", "yaw attitude"};
 INNAME = {"longitudinal control angle", "lateral control angle"};
 sys = ss(A, B, C, D, Tsam, Ncontin, Ndiscr, STNAME, INNAME, STNAME);
 
 % TODO: figure out how to apply a step (i.e. what is meaningful to step?)
+figure; step(sys)
+figure; step(sys, 2)
