343ed132143070b942d39cb171ca7aed22ee6ef7
Andrew Cameron <amcameron@gmail.com>
2010-12-07 09:37:12 -0500

    suppress output and make it MATLAB-compatible
    MATLAB's end statements are less descriptive than Octave's (there's no
    "endfunction" or "endif"; all simply use "end").  Also, line
    continuations are performed with a "..." instead of a "\" at EOL.  This
    is compatible with Octave, so no harm there.
    
    Also, the differential equation function must accept TWO arguments in
    MATLAB: y' = f(t, y).  You don't have to do anything with the t, so I
    just introduced it so it works with ode45 (and, presumably, other ODE
    solvers).

--- sim3d.m
+++ sim3d.m
@@ -9,40 +9,41 @@
 
 % Xdot = f(X) - calculate the derivatives of the state vector given the current
 % state vector. suitable for running through an ode solver.
-function Xdot = f(X)
-    P    = planpath(X)
-    x    = localize(X)
-    p    = localizeplan(X, P)
-    d    = controller(x, p)
-    Xdot = plant(X, d)
-    Xdot = Xdot'
-endfunction
+function Xdot = f(t, X)
+    P    = planpath(X);
+    x    = localize(X);
+    p    = localizeplan(X, P);
+    d    = controller(x, p);
+    Xdot = plant(X, d);
+    Xdot = Xdot';
+end
 
 % P = planpath(x) - plan a global frame path to the origin given the current
 % state vector.
 function P = planpath(X)
     % naive linear path planning
     % (δZ/δX)* = Z/X
-    P(1) = X(3)/X(1)
+    P(1) = X(3)/X(1);
 
     % logarithmic descent rate (arbitrary coefficients for the moment)
     % Zdot* =~ log(Z + 1)
     % Zdot* = 0 when Z = 0
-    a = 1
-    b = 1000
-    P(2) = a*log(X(3)/b + 1)
-endfunction
+    a = 1;
+    b = 1000;
+    P(2) = a*log(X(3)/b + 1);
+end
 
 % p = localizeplan(X, P) - create a local frame version of a global frame plan
 function p = localizeplan(X, P)
-    Zdotstar = P(2)
-    Xdotstar = P(2)/P(1) % Xdot = Zdot/(δZ/δX) [chain rule]
-    p = R(X(7), X(8), X(9)) * [ Xdotstar; 0; Zdotstar ]
-endfunction
+    Zdotstar = P(2);
+    Xdotstar = P(2)/P(1); % Xdot = Zdot/(δZ/δX) [chain rule]
+    p = R(X(7), X(8), X(9)) * [ Xdotstar; 0; Zdotstar ];
+end
 
 function x = localize(X)
-    x(1:3) = R(X(7), X(8), X(9)) * X(4:6)'
-endfunction
+    x(1:3) = R(X(7), X(8), X(9)) * X(4:6);
+    x(4:5) = [ X(8) X(11)];
+end
 
 function d = controller(x, p)
     x2d = [ x(1) x(3) ];
@@ -56,10 +57,10 @@ endfunction
 % create a forward rotation matrix (global -> local)
 % uses 3-2-1 euler angles
 function A = R(phi, theta, psi)
-    A = [ 1 0 0; 0 cos(phi) sin(phi); 0 -sin(phi) cos(phi) ] * \
-	[ cos(theta) 0 -sin(theta); 0 1 0; sin(theta) 0 cos(theta) ] * \
-	[ cos(psi) sin(psi) 0; -sin(psi) cos(psi) 0; 0 0 1 ]
-endfunction
+    A = [ 1 0 0; 0 cos(phi) sin(phi); 0 -sin(phi) cos(phi) ] * ...
+    [ cos(theta) 0 -sin(theta); 0 1 0; sin(theta) 0 cos(theta) ] * ...
+    [ cos(psi) sin(psi) 0; -sin(psi) cos(psi) 0; 0 0 1 ];
+end
 
 % Xdot = plant(X, d) - model the plant
 % given the state vector and the control input, determine the rate of change of
@@ -103,20 +104,20 @@ function Xdot = plant(X, d)
     mw  = 31;    % wing mass [kg]
     lwp = 1.2;   % distance from wing to pilot [m]
 
-    m   = mp + mw % total mass [kg]
-    lcw = mp/m * lwp % distance between wing and CoM [m]
+    m   = mp + mw; % total mass [kg]
+    lcw = mp/m * lwp; % distance between wing and CoM [m]
 
     % forces & moments
-    L  = Cl(attack, true_airspeed) * 1/2 * rho * true_airspeed^2 * S
-    D  = Cd(attack, true_airspeed) * 1/2 * rho * true_airspeed^2 * S
-    Mp = Cm(attack, true_airspeed) * 1/2 * rho * true_airspeed^2 * S * c
-    F  = (mp + mw)*g*ug \
-         - L*uaz \
-         - D*uax \
-         + Mp*lcw*cross(uay, upw)
-    M  = Mp*uay \
-	- L*lcw*cross(upw, uaz) \
-	- D*lcw*cross(upw, uax)
+    L  = Cl(attack, true_airspeed) * 1/2 * rho * true_airspeed^2 * S;
+    D  = Cd(attack, true_airspeed) * 1/2 * rho * true_airspeed^2 * S;
+    Mp = Cm(attack, true_airspeed) * 1/2 * rho * true_airspeed^2 * S * c;
+    F  = (mp + mw)*g*ug ...
+         - L*uaz ...
+         - D*uax ...
+         + Mp*lcw*cross(uay, upw);
+    M  = Mp*uay ...
+    - L*lcw*cross(upw, uaz) ...
+    - D*lcw*cross(upw, uax);
 
     % position derivatives given by speeds
     Xdot(1:3) = X(4:6);
@@ -127,10 +128,10 @@ function Xdot = plant(X, d)
     % M = Iα (along primary axes only)
     % XXX HORRIBLE ASSUMPTION 
     % model glider as a sphere (great flying spheres of mathland!)
-    Xdot(10) = M(1)/(2/5*m*lcw^2)
-    Xdot(11) = M(2)/(2/5*m*lcw^2)
-    Xdot(12) = M(3)/(2/5*m*lcw^2)
-endfunction
+    Xdot(10) = M(1)/(2/5*m*lcw^2);
+    Xdot(11) = M(2)/(2/5*m*lcw^2);
+    Xdot(12) = M(3)/(2/5*m*lcw^2);
+end
 
 % coefficient of lift (only considers incidence ATM)
 function coef = Cl(alpha, v)
@@ -141,32 +142,32 @@ function coef = Cl(alpha, v)
     m =  2.4911;
     b = -0.30435;
 
-    coef = m * alpha + b
-endfunction
+    coef = m * alpha + b;
+end
 
 % coefficient of drag (only considers incidence ATM)
 function coef = Cd(alpha, v)
     % piecewise-quadratic approximation of drag curve
     % (sampled at 0deg, 7deg, 15deg, slope @ 7deg fixed at 0)
-    a = 0.24555
-    b = -0.060
-    c = 0.08
-    e = 0.14324
-    f = -0.035
-    g = 0.069346
-    if (alpha <= 7*(pi/180))
-	coef = a*alpha^2 + b*alpha + c
+    a = 0.24555;
+    b = -0.060;
+    c = 0.08;
+    e = 0.14324;
+    f = -0.035;
+    g = 0.069346;
+    if (alpha <= 7*(pi/180));
+        coef = a*alpha^2 + b*alpha + c;
     else
-	coef = d*alpha^2 + e*alpha + f
-    endif
-endfuction
+        coef = d*alpha^2 + e*alpha + f;
+    end
+end
 
 % pitching coefficient (only considers incidence ATM)
 function coef = Cm(alpha, v)
     % quadratic approximation of pitching curve
     % (sampled at 0deg, 15deg, and 30deg)
-    a = 0.076394
-    b = -0.06
-    c = 0.04
-    coef = a*alpha^2 + b*alpha + c
-endfunction
+    a = 0.076394;
+    b = -0.06;
+    c = 0.04;
+    coef = a*alpha^2 + b*alpha + c;
+end
