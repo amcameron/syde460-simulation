5b4b5d04b115f0466d94ded523da7992ff7fd503
Andrew Cameron <amcameron@gmail.com>
2011-03-07 18:37:22 -0500

    update simplant to find full-state controller gains, start implementing

--- simplant.m
+++ simplant.m
@@ -22,10 +22,11 @@ Nu_long = N_long(size(B_long, 2), :);
 % Try pole placement: for overshoot <= 5% and settling time <= 4s, we have
 % theta >= 43.6° and sigma <= -1
 % So, try for poles at -2+j, -2-j, -10, -15
-P_long = [-2+j, -2-j, -10, -15];
-K_long = place(A_long, B_long, P_long);
-disp('P:'), disp(P_long)
-disp('eig(A - BK):'), disp(eig(A_long - B_long*K_long))
+%P_long = [-2+j, -2-j, -10, -15];
+%K_long = place(A_long, B_long, P_long);
+%disp('P:'), disp(P_long)
+%disp('eig(A - BK):'), disp(eig(A_long - B_long*K_long))
+%disp('K:'), disp(K_long)
 
 % Lateral equations:
 A_lat = [-0.2195  -0.1580 -10.7980 9.722 -1.3098;
@@ -35,22 +36,56 @@ A_lat = [-0.2195  -0.1580 -10.7980 9.722 -1.3098;
           0        0        1      0      0];
 B_lat = [0; 3.6136; -0.4311; 0; 0];
 C_lat = eye(5); % output all state variables for now
-sys_lat = ss(A_lat, B_lat, C_lat);
+D_lat = [];
+sys_lat = ss(A_lat, B_lat, C_lat, D_lat);
 
 % Entire system all at once!
 A = blkdiag(A_long, A_lat);
 B = blkdiag(B_long, B_lat);
 C = eye(9); % output all state variables for now
-D = [];
-Tsam = 0;
-Ncontin = rows(A);
-Ndiscr = 0;
-STNAME = {"axial velocity", "normal velocity", "pitch rate", ...
-        "pitch attitude", "lateral velocity", "roll rate", "yaw rate", ...
-        "roll attitude", "yaw attitude"};
-INNAME = {"longitudinal control angle", "lateral control angle"};
-sys = ss(A, B, C, D, Tsam, Ncontin, Ndiscr, STNAME, INNAME, STNAME);
+D = zeros(9, 2);
+%Tsam = 0;
+%Ncontin = rows(A);
+%Ndiscr = 0;
+STNAME = {'axial velocity', 'normal velocity', 'pitch rate', ...
+        'pitch attitude', 'lateral velocity', 'roll rate', 'yaw rate', ...
+        'roll attitude', 'yaw attitude'};
+INNAME = {'longitudinal control angle', 'lateral control angle'};
+sys = ss(A, B, C, D);%, Tsam, Ncontin, Ndiscr, STNAME, INNAME, STNAME);
+
+N = [A B; C D]\[zeros(9, 9); ones(9, 9)];
+Nx = N(1:size(A, 1), :);
+Nu = N(size(B, 2), :);
+
+P = [-2+j, -2-j, -10, -15, -20, -25, -30, -35, -40];
+K = place(A, B, P);
+disp('P:'), disp(P')
+disp('eig(A - BK):'), disp(eig(A - B*K))
+disp('K:'), disp(K)
 
 % TODO: figure out how to apply a step (i.e. what is meaningful to step?)
-figure; step(sys)
-figure; step(sys, 2)
+%figure; step(sys, 10) % first 10 seconds of step response
+
+%% Prepare Nyquist and pole-zero plots for each input-state TF
+%for input=1:size(B, 2)
+%	subB = B(:, input);
+%	for state=1:size(C, 1)
+%		subC = zeros(1, size(C, 1));
+%		subC(state) = 1;
+%		subsys = ss(A, subB, subC, []);
+%		if ((input == 1) && (state < 5)) || ((input == 2) && (state > 4))
+%			figure; nyquist(subsys);
+%			title([INNAME{input} ' to ' STNAME{state} ' (Nyquist)']);
+%			print(sprintf('figs/%d%dnyq.eps', input, state));
+%			figure; pzmap(subsys);
+%			title([INNAME{input} ' to ' STNAME{state} ' (pzmap)']);
+%			print(sprintf('figs/%d%dpz.eps', input, state));
+%			close all;
+%			disp('Poles:')
+%			pole(subsys)
+%			disp('Zeroes:')
+%			zero(subsys)
+%			pause
+%		end
+%	end
+%end
