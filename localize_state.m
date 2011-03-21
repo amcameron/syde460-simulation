c95fb58580d94c28c6b6ba2bccefc417b16c9114
Peter Lobsinger <plobsing@gmail.com>
2011-03-21 13:04:09 -0400

    correct angle position localization
    NOTE: angle disturbance is not observable - design controller accordingly

--- localize_state.m
+++ localize_state.m
@@ -2,7 +2,7 @@ function x = localize_state(X)
     Rb       = R(X(7), X(8), X(9));
     x(1:3)   = Rb * X(1:3);
     x(4:6)   = Rb * X(4:6);
-    x(7:9)   = -X(7:9);
+    x(7:9)   = [0 0 0];
     x(10:12) = Rb * X(10:12);
 end
 
