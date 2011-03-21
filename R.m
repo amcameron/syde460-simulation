6281d44447ed654dd1e4210373b050c9b980e3a8
Peter Lobsinger <plobsing@gmail.com>
2011-03-21 11:33:52 -0400

    rewrite path planner to maintain horizontal orientations

--- R.m
+++ R.m
@@ -1,5 +1,5 @@
 % create a forward rotation matrix (global -> local)
-% uses 3-2-1 euler angles
+% uses 3-2-1 euler angles (yaw-pitch-roll)
 function A = R(phi, theta, psi)
     A = [ 1 0 0; 0 cos(phi) sin(phi); 0 -sin(phi) cos(phi) ] * ...
     [ cos(theta) 0 -sin(theta); 0 1 0; sin(theta) 0 cos(theta) ] * ...
--- planpath.m
+++ planpath.m
@@ -1,33 +1,43 @@
+% use gradient method to accomplish a number of prioritized objectives
 function P = planpath(X)
-    % first priority - align craft orientation with trajectory
-    utraj = X(4:6)/norm(X(4:6));
-    alignedness = dot(utraj, R(X(7), X(8), X(9)) * [1 0 0]');
-    if alignedness < cos(pi/16)
-	% alignment outside +-~10 degrees of trajectory
-	% decide how to get to trajectory window, banking, pitching, or both
-	d_align_by_d_yaw   = dot(utraj, R(X(7) + pi/360, X(8), X(9)) * [1 0 0]') - alignedness;
-	d_align_by_d_pitch = dot(utraj, R(X(7), X(8) + pi/360, X(9)) * [1 0 0]') - alignedness;
-	% magnitude of angle change we need (approximated by 1-cos(x)=x for small x)
-	magnitude  = 1 - alignedness;
-	deriv_mag  = norm([d_align_by_d_pitch d_align_by_d_yaw]);
-	pitch_star = magnitude * d_align_by_d_pitch/deriv_mag;
-	yaw_star   = magnitude * d_align_by_d_yaw/deriv_mag;
+    % gradient method constants:
+    delt = pi/180; % step size for derivative estimation
+    step = pi/16;  % step size for gradient method
+
+    % important vectors trajectory and orientation
+    utraj             = X(4:6)/norm(X(4:6));
+    udir              = R(X(7), X(8), X(9)) * [1 0 0]';
+    udir_plus_d_yaw   = R(X(7) + delt, X(8), X(9)) * [1 0 0]';
+    udir_plus_d_pitch = R(X(7), X(8) + delt, X(9)) * [1 0 0]';
+
+    % first priority - stay level
+    verticality = dot(udir, [0 0 1]);
+    if verticality > sin(pi/16)
+	d_by_d_yaw   = (1/delt)*(dot(udir_plus_d_yaw,   [0 0 1]) - verticality)
+	d_by_d_pitch = (1/delt)*(dot(udir_plus_d_pitch, [0 0 1]) - verticality)
+	yaw_star     = X(7) - step*d_by_d_yaw;
+	pitch_star   = X(8) - step*d_by_d_pitch;
     else
-	% craft is in nominally aligned flight-envelope, attempt manoeuvres towards origin
-	uorig = -X(1:3)/norm(X(1:3));
-	alignedness = dot(uorig, R(X(7), X(8), X(9)) * [1 0 0]');
-	% decide how to point towards origin, banking, pitching, or both
-	d_align_by_d_yaw   = dot(uorig, R(X(7) + pi/360, X(8), X(9)) * [1 0 0]') - alignedness;
-	d_align_by_d_pitch = dot(uorig, R(X(7), X(8) + pi/360, X(9)) * [1 0 0]') - alignedness;
-	% magnitude of angle change we need (approximated by 1-cos(x)=x for small x)
-	magnitude  = 1 - alignedness;
-	deriv_mag  = norm([d_align_by_d_pitch d_align_by_d_yaw]);
-	pitch_star = magnitude * d_align_by_d_pitch/deriv_mag;
-	yaw_star   = magnitude * d_align_by_d_yaw/deriv_mag;
+	% second priority - align craft orientation with trajectory
+	alignedness = dot(utraj, udir);
+	if alignedness < cos(pi/16)
+	    d_by_d_yaw   = (1/delt)*(dot(utraj, udir_plus_d_yaw)   - alignedness);
+	    d_by_d_pitch = (1/delt)*(dot(utraj, udir_plus_d_pitch) - alignedness);
+	    yaw_star   = X(7) + step*d_by_d_yaw;
+	    pitch_star = X(8) + step*d_by_d_pitch;
+	else
+	    % third priority - point towards target (origin)
+	    uorig = -X(1:3)/norm(X(1:3));
+	    alignedness = dot(uorig, udir);
+	    d_by_d_yaw   = (1/delt)*(dot(uorig, udir_plus_d_yaw)   - alignedness);
+	    d_by_d_pitch = (1/delt)*(dot(uorig, udir_plus_d_pitch) - alignedness);
+	    yaw_star   = X(7) + step*d_by_d_yaw;
+	    pitch_star = X(8) + step*d_by_d_pitch;
+	end
     end
 
     % we have control of pitch and roll, but pitch and yaw are how trajectory works
-    % roll by negative yaw target, yaw becomes pitch, and (hopefully) things work themselves out
-    P = [pitch_star, -yaw_star];
+    % roll by negative scaled yaw target, yaw becomes pitch, and (hopefully) things work themselves out
+    P = [pitch_star, -0.2*yaw_star];
 end
 
