00e0a437434c06ea55c9016984ad358c24667895
Andrew Cameron <amcameron@gmail.com>
2011-03-26 12:56:59 -0400

    fix bug in controller wrt ordering of states

--- controller.m
+++ controller.m
@@ -4,7 +4,7 @@ function d = controller(x, z)
     % Adjust the pitch by the steady-state value to get the disturbance pitch.
     % Steady-state values for roll & yaw are zero.
     % TODO: put disturbance pitch adjustment in localize_state
-    x3d = [ x(4) x(6) x(11) (x(8)+0.2621) x(5) x(10) x(12) x(9) x(7) ]';
+    x3d = [ x(4) x(6) x(11) (x(8)+0.2621) x(5) x(12) x(10) x(9) x(7) ]';
 
     persistent A B C H Kstate Kref;
     if (isempty(A))
