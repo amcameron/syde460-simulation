f3f264c16f8098539b11627292645ab3d74eee7c
Peter Lobsinger <plobsing@gmail.com>
2011-04-02 20:05:16 -0400

    anles are ordered 3-2-1 (psi, theta, phi), not 1-2-3

--- R.m
+++ R.m
@@ -1,6 +1,6 @@
 % create a forward rotation matrix (global -> local)
 % uses 3-2-1 euler angles (yaw-pitch-roll)
-function A = R(phi, theta, psi)
+function A = R(psi, theta, phi)
     A = [ 1 0 0; 0 cos(phi) sin(phi); 0 -sin(phi) cos(phi) ] * ...
     [ cos(theta) 0 -sin(theta); 0 1 0; sin(theta) 0 cos(theta) ] * ...
     [ cos(psi) sin(psi) 0; -sin(psi) cos(psi) 0; 0 0 1 ];
